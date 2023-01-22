f42ffe077b205ad658f6c74c1176463d1d85e7dc
Geo Bucay <ebucayle@pichincha.com>
2023-01-22 14:08:54 -0500

    Producto service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,24 @@
+import { Product } from './../../models/product.model';
+import { ProductsService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private serviceProducts: ProductsService) {}
 
   ngOnInit(): void {
+    this.getAllProdutcs();
   }
 
+  getAllProdutcs() {
+    this.serviceProducts.getAllSimple().subscribe((product) => {
+      this.products = product;
+    });
+  }
 }
